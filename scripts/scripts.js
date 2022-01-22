5f8470de617e0b114ca27571912e806f0a08975a
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-01-22 14:33:39 +0300

    feat: создать скрипты

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,12 +4,41 @@ const popupCloseButton = document.querySelector('.popup__close-button');
 
 function openPopup () {
     popup.classList.add('popup_opened');
-    console.log('работает');
-}
+};
 
 function closePopup () {
     popup.classList.remove('popup_opened');
-    console.log('отдыхает'); 
-}
+};
 
 profileOpenPopupButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__edit_el_name');
+let jobInput = document.querySelector('.popup__edit_el_about-me');
+
+let profileName = document.querySelector('.profile__info-name');
+let profileJob =  document.querySelector('.profile__info-job');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault ();
+
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+};
+
+formElement.addEventListener('click', formSubmitHandler);
+
+let buttonLikes = document.querySelectorAll('.elements__button');
+let like = document.getElementById('');
+
+function putLike (evt) {
+    evt.preventDefault ();
+    evt.target.classList.toggle('elements__button_theme_dark');    
+};
+
+buttonLikes.forEach(
+        function(element) {
+        element.addEventListener('click', putLike);
+    }
+);
