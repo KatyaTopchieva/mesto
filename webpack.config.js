ff2f1cc80d20265296c22045dd9c44dfcc544e86
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-04-01 12:49:43 +0300

    "fix: исправить настройки webpack.config"

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    filename: 'main.js',
     publicPath: '',
   },
   mode: 'development',
@@ -43,7 +43,6 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'src/index.html',
       template: 'src/index.html'
     }),
     new CleanWebpackPlugin(),
